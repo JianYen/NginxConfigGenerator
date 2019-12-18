85e4f1966f7c23de36cf6788712cd5ca930d7816
TGFC065 <tgfc065@tgfc.tw>
2019-12-18 13:58:42 +0800

    git init

---
+++ src/components/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item active>Active</b-nav-item>
+      <b-nav-item>Link</b-nav-item>
+      <b-nav-item>Another Link</b-nav-item>
+      <b-nav-item disabled>Disabled</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
